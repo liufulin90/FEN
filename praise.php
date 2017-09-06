bc84ebb0b13f3eaf398c5e8fd986ad27ad042bd5
liufulin90 <liufulin90@163.com>
2017-09-06 11:25:54 +0800

    add praise set data adn images

--- praise.php
+++ praise.php
@@ -3,19 +3,16 @@ header("Content-type: application/json; charset=utf-8");
 
 require_once 'RedisCmnCache.php';
 try {
-    $VISITOR_TIMES_KEY = 'fen_visitor_times';
     $PRAISE_TIMES_KEY = 'fen_praise_times';
     $cmnCache = new RedisCmnCache();
 
-    $visitorTimes = $cmnCache->get($VISITOR_TIMES_KEY);
-    if ($visitorTimes) {
-        $cmnCache->set($VISITOR_TIMES_KEY, ++$visitorTimes);
-    } else {
-        $cmnCache->set($VISITOR_TIMES_KEY, 1);
-    }
     $praiseTimes = $cmnCache->get($PRAISE_TIMES_KEY);
-    $praiseTimes = $praiseTimes ? $praiseTimes : 0;
-    echo responseData(array('visitor' => $visitorTimes, 'praise' => $praiseTimes));
+    if ($praiseTimes) {
+        $cmnCache->set($PRAISE_TIMES_KEY, ++$praiseTimes);
+    } else {
+        $cmnCache->set($PRAISE_TIMES_KEY, 1);
+    }
+    echo responseData(array('praise' => $praiseTimes));
 } catch(Exception $e){
     echo responseData($e->getPrevious(), $e->getCode(), $e->getMessage());
 }
